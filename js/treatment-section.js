03b77bbff839807e53ff291fecd6fa8bea4a9803
Jan Denver Gimenez <iamjd98@gmail.com>
2023-06-12 10:42:32 +0800

    modified:   index.html 	modified:   js/treatment-section.js

--- js/treatment-section.js
+++ js/treatment-section.js
@@ -2,19 +2,24 @@ let listVideo = document.querySelectorAll('.video-list .vid');
 let mainVideo = document.querySelector('.main-video video');
 let title = document.querySelector('.main-video .title');
 
-
-listVideo.forEach(video =>{
-    video.onclick = () =>{
-        listVideo.forEach(vid => vid.classList.remove('active'));
-        video.classList.add('active');
-        if(video.classList.contains('active')){
-            let src = video.children[0].getAttribute('src');
-            mainVideo.src = src;
-            let text = video.children[1].innerHTML;
-            title.innerHTML = text;
-        };
-    };
+listVideo.forEach(video => {
+  video.onclick = () => {
+    listVideo.forEach(vid => vid.classList.remove('active'));
+    video.classList.add('active');
+    
+    if (video.classList.contains('active')) {
+      let src = video.children[0].getAttribute('src');
+      mainVideo.src = src;
+      
+      let text = video.children[1].innerHTML;
+      title.innerHTML = text;
+      
+      // Reset the poster attribute of the main video
+      mainVideo.removeAttribute('poster');
+    }
+  };
 });
+
 
 var video = document.getElementById("myVideo");
 var playButton = document.getElementById("playButton");
